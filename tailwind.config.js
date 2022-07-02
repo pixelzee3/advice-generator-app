48b594775ab7df222e7988ccb23b668dfd6bc957
pixelzee3 <bpixelzee@gmail.com>
2022-07-02 18:07:15 +0700

    Define project styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Manrope', 'sans-serif', 'ui-sans-serif', 'system-ui'],
+    },
+    extend: {
+      colors: {
+        'light-cyan': 'hsl(193, 38%, 86%)',
+        'neon-green': 'hsl(150, 100%, 66%)',
+        'grayish-blue': 'hsl(217, 19%, 38%)',
+        'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+        'dark-blue': 'hsl(218, 23%, 16%)',
+      },
+    },
   },
   plugins: [],
-}
+};

2a872e98ab570020fa198882085b19e389e2c7e2
pixelzee3 <bpixelzee@gmail.com>
2022-07-02 18:00:55 +0700

    Define page head.

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <Head>
+      <Title>Advice Generator App</Title>
+      <Meta name="description" content="Generate random advice!" />
+      <Link
+        rel="shortcut icon"
+        href="/images/favicon-32x32.png"
+        type="image/x-icon"
+      />
+    </Head>
     <h1 class="text-2xl">Hello World</h1>
   </div>
 </template>
